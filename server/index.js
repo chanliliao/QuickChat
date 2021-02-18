5e02f77adfaa6eec38644138f2531d2a4a5374db
Chan Li Liao <cliao119@gmail.com>
2021-02-17 23:08:23 -0500

    socketio set up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import router from './router.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,7 +13,13 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  // ...
+  console.log('We have a new connection!');
+
+  socket.on('disconnect', () => {
+    console.log('User had left!');
+  });
 });
+
+app.use('/', router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
