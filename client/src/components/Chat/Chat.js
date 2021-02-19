3c45a09e8251f71bc3991d9db72326b144c38d29
Chan Li Liao <cliao119@gmail.com>
2021-02-18 20:06:13 -0500

    connect socket.io

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import { io } from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    let socket = io(ENDPOINT, {
+      transports: ['websocket', 'polling', 'flashsocket'],
+    });
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, () => {});
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
   return <div>Chat</div>;
 };
 
--- server/index.js
+++ server/index.js
@@ -9,11 +9,19 @@ const app = express();
 
 const server = createServer(app);
 const io = new Server(server, {
-  // ...
+  cors: {
+    origin: 'your origin',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
 });
 
 io.on('connection', (socket) => {
   console.log('We have a new connection!');
+
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room);
+  });
 
   socket.on('disconnect', () => {
     console.log('User had left!');
