d76247495b3aa6f2353391a181749c9c2135383d
Chan Li Liao <cliao119@gmail.com>
2021-02-24 21:58:10 -0500

    changed name

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://react-socketio-online-chat.herokuapp.com/';
+  const ENDPOINT = 'https://quickchatbackend.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
