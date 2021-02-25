013d0744fbb3e22775e9f039a0a33438ac2d2af5
Chan Li Liao <cliao119@gmail.com>
2021-02-24 21:49:45 -0500

    added new scroll down function

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import ScrollToBottom from 'react-scroll-to-bottom';
+import React, { useEffect, useRef } from 'react';
 import Message from './Message/Message';
 
 import './Messages.css';
 
 const Messages = ({ messages, name }) => {
+  const msgEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    msgEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <ScrollToBottom className='messages'>
+    <div className='messages'>
       {messages.map((message, i) => (
         <div key={i}>
           <Message message={message} name={name} />
         </div>
       ))}
-    </ScrollToBottom>
+      <div ref={msgEndRef} />
+    </div>
   );
 };
 
